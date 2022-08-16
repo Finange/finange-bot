d9d964514e69e97b027c6bd2507ba1347e8f988b
Atilio <atilio.jesus@grupoboticario.com.br>
2022-08-16 19:13:24 -0300

    Get started to coding bot's core

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Lindo")
+import requests
+import json
+import time
+import os
+
+while True:
+    token = os.environ.get("TOKEN")
+    url_base = f'https://api.telegram.org.bot{token}/getUpdates'
+
+    resultado = requests.get(url_base)
+
+    print(resultado.json())
+    time.sleep(10) 
