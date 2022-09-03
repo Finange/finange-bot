0364968c004d5d6cfaece20ff1f37c94fef17836
Atilio <atilio.jesus@grupoboticario.com.br>
2022-09-03 16:25:44 -0300

    Deleting core to creating everything in 1 file

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-import requests
-import json
 import time
-import os
-
-
-while True:
-    token = os.environ.get("TOKEN")
-    url_base = f'https://api.telegram.org/bot{token}/getUpdates'
-
-    resultado = requests.get(url_base)
-
-    try:
-        print(resultado.json())
-    except requests.exceptions.ConnectionError:
-        print("Error : {}".format(resultado.status_code))
-    time.sleep(3) 
+import requests
+import os 
