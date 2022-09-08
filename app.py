9f55cc707b3b981ce05b841c97a8cefbfa7d448a
Atilio <atilio.jesus@grupoboticario.com.br>
2022-09-08 16:49:17 -0300

    Nova estrutura funcionando e interagindo com o bot

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import time
 import requests
 import os 
-import telebot
+import telegram.ext
+
+API_TOKEN = os.environ.get("API_TOKEN")
+
+def start(update, context):
+    update.message.reply_text("Hey! Como posso ajudar você?")
+
+updater = telegram.ext.Updater(API_TOKEN, use_context=True)
+disp = updater.dispatcher
+
+disp.add_handler(telegram.ext.CommandHandler("start", start))
+
+updater.start_polling()
+updater.idle()
