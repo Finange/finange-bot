8ed28815c718dd1082716000363119d05887e3c2
Atilio <atilio.jesus@grupoboticario.com.br>
2022-09-08 17:00:49 -0300

    Add new files with tax math

---
+++ app/tax/brazil/impostos.py
@@ -0,0 +1,10 @@
+def calculo_imposto_de_renda(update, context):
+    update.message.reply_text("""
+    
+    Hmmm! Então você quer que eu calcule seu imposto de renda?
+
+    Ok! Vamos lá.
+
+    Me diga, quanto você ganha?
+    
+    """)
